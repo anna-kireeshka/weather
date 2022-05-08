7109a8eff3d60412dc1a04f4633141a87a074bbc
Anna <kireeva625@gmail.com>
2022-05-08 12:57:33 +0400

    MainPage

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
-
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  background-color: #fffaf5;
+  height: 100vh;
+}
 </style>
